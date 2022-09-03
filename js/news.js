4b79077c559615bff1c52e133f9d4f46461f8a6f
arifusinbad <ariful.islam.sinbad@gmail.com>
2022-09-03 23:05:38 +0600

    commit 3

--- js/news.js
+++ js/news.js
@@ -1,2 +1,10 @@
 
+const loadCategory = () =>{
+ fetch('https://openapi.programming-hero.com/api/news/categories')
+ .then(res => res.json())
+ .then(data => displayCategory(data.data.news_category))
+}
+
+
+
 
